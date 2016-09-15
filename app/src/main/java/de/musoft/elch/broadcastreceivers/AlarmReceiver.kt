02d80056c4617782298027ab88698fe5bc1f544d
Uli Luckas <luckas@musoft.de>
2016-09-15 20:59:35 +0200

    Factor ringing out of broadcast receiver into seperate file

--- app/src/main/java/de/musoft/elch/broadcastreceivers/AlarmReceiver.kt
+++ app/src/main/java/de/musoft/elch/broadcastreceivers/AlarmReceiver.kt
@@ -4,14 +4,8 @@ import android.app.PendingIntent
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
-import android.media.AudioManager
-import android.media.MediaPlayer
-import android.os.PowerManager
-import com.example.elch.app.R
-import de.musoft.elch.extensions.powerManager
-import de.musoft.elch.extensions.uriForRessource
+import de.musoft.elch.media.ring
 
-private const val volume = 1.0f
 private const val ELCH_ACTION = "de.musoft.elch.alarm.ELCH_ALARM"
 
 public fun getAlarmIntent(context: Context, flags: Int): PendingIntent {
@@ -20,49 +14,10 @@ public fun getAlarmIntent(context: Context, flags: Int): PendingIntent {
     return PendingIntent.getBroadcast(context, 0, intent, flags)
 }
 
-/**
- * Call prepareAsync on mediaPlayer, catch all exceptions and report success
- *
- * @param mediaPlayer The mediaplayer object
- *
- * @return true if successfull
- */
-private fun tryPrepareAsync(mediaPlayer: MediaPlayer): Boolean {
-    try {
-        mediaPlayer.prepareAsync()
-        return true
-    } catch (e: Throwable) {
-        return false
-    }
-}
-
 class AlarmReceiver : BroadcastReceiver() {
 
     override fun onReceive(context: Context, intent: Intent) {
-        val uri = context.uriForRessource(R.raw.ring)
-        val mediaPlayer = MediaPlayer()
-        val pm = context.powerManager
-        val wakeLock = pm.newWakeLock(PowerManager.PARTIAL_WAKE_LOCK, "AlarmHandover")
-
-        mediaPlayer.setOnCompletionListener { it.release() }
-        mediaPlayer.setWakeMode(context, PowerManager.PARTIAL_WAKE_LOCK)
-        mediaPlayer.setDataSource(context, uri)
-        mediaPlayer.setAudioStreamType(AudioManager.STREAM_ALARM)
-        mediaPlayer.setVolume(volume, volume)
-        mediaPlayer.setOnPreparedListener {
-            try {
-                mediaPlayer.start()
-            } finally {
-                // No more need to hold the WakeLock
-                // While playing, the MediaPlayer holds it's own WakeLock
-                wakeLock.release()
-            }
-        }
-        if (tryPrepareAsync(mediaPlayer)) {
-            // If prepareAsync succeeded, we can be sure, the onPreparedListener will be called
-            // So proceede to acquire the WakeLock, it will be released in the onPreparedListener
-            wakeLock.acquire()
-        }
+        ring(context)
     }
 
 }
---
+++ app/src/main/java/de/musoft/elch/media/Ringer.kt
@@ -0,0 +1,58 @@
+package de.musoft.elch.media
+
+import android.content.Context
+import android.media.AudioManager
+import android.media.MediaPlayer
+import android.os.PowerManager
+import com.example.elch.app.R
+import de.musoft.elch.extensions.powerManager
+import de.musoft.elch.extensions.uriForRessource
+
+/**
+ * Created by luckas on 15.09.16.
+ */
+
+private const val volume = 1.0f
+
+/**
+ * Call prepareAsync on mediaPlayer, catch all exceptions and report success
+ *
+ * @param mediaPlayer The mediaplayer object
+ *
+ * @return true if successfull
+ */
+private fun tryPrepareAsync(mediaPlayer: MediaPlayer): Boolean {
+    try {
+        mediaPlayer.prepareAsync()
+        return true
+    } catch (e: Throwable) {
+        return false
+    }
+}
+
+fun ring(context: Context) {
+    val uri = context.uriForRessource(R.raw.ring)
+    val mediaPlayer = MediaPlayer()
+    val pm = context.powerManager
+    val wakeLock = pm.newWakeLock(PowerManager.PARTIAL_WAKE_LOCK, "AlarmHandover")
+
+    mediaPlayer.setOnCompletionListener { it.release() }
+    mediaPlayer.setWakeMode(context, PowerManager.PARTIAL_WAKE_LOCK)
+    mediaPlayer.setDataSource(context, uri)
+    mediaPlayer.setAudioStreamType(AudioManager.STREAM_ALARM)
+    mediaPlayer.setVolume(volume, volume)
+    mediaPlayer.setOnPreparedListener {
+        try {
+            mediaPlayer.start()
+        } finally {
+            // No more need to hold the WakeLock
+            // While playing, the MediaPlayer holds it's own WakeLock
+            wakeLock.release()
+        }
+    }
+    if (tryPrepareAsync(mediaPlayer)) {
+        // If prepareAsync succeeded, we can be sure, the onPreparedListener will be called
+        // So proceede to acquire the WakeLock, it will be released in the onPreparedListener
+        wakeLock.acquire()
+    }
+}
