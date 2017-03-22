95e67e285441d836b1b32807d88a1efb8d4c8263
Uli Luckas <uli.luckas@sinnerschrader-mobile.com>
2017-03-22 22:58:10 +0100

    Intermediate state

--- app/src/main/java/de/musoft/elch/alarm/AlarmTimer.kt
+++ app/src/main/java/de/musoft/elch/alarm/AlarmTimer.kt
@@ -3,33 +3,18 @@ package de.musoft.elch.alarm
 import android.app.AlarmManager
 import android.app.PendingIntent
 import android.content.Context
-import android.content.SharedPreferences
 import android.os.Build
 import android.os.Handler
 import android.os.Looper
-import android.os.SystemClock
 import de.musoft.elch.broadcastreceivers.getAlarmIntent
-import de.musoft.elch.delegates.BooleanSharedPreferenceShadow
-import de.musoft.elch.delegates.LongSharedPreferenceShadow
 import de.musoft.elch.extensions.alarmManager
-import de.musoft.elch.extensions.mToMs
-import de.musoft.elch.extensions.msToS
 import java.util.*
-import kotlin.reflect.KProperty
 
 /**
  * Created by luckas on 1/29/15.
  */
 
-private const val SPIELZEIT_DURATION_MIN: Long = 8L
-private const val KEY_TIMER_RUNNING = "TIMER_RUNNING"
-private const val KEY_ALARM_TIME_MS = "ALARM_TIME_MS"
-private const val KEY_REMAINING_TIME_MS = "REMAINING_TIME_MS"
-
 private const val S_IN_MS = 1000L
-
-private val SPIELZEIT_DURATION_MS = SPIELZEIT_DURATION_MIN.mToMs()
-
 
 class AlarmTimer(private val applicationContext: Context) {
 
@@ -37,40 +22,18 @@ class AlarmTimer(private val applicationContext: Context) {
     private val secondsChangedCallbacks = ArrayList<(Long) -> Unit>()
     private var secondsChangeTimer: Timer? = null
     private val alarmManager: AlarmManager = applicationContext.alarmManager
-    private val sharedPreferences: SharedPreferences = applicationContext.getSharedPreferences("TimerState", Context.MODE_PRIVATE)
-    private var alarmTimeMS: Long by LongSharedPreferenceShadow(sharedPreferences, KEY_ALARM_TIME_MS, 0)
-    private var countdownRunning: Boolean by BooleanSharedPreferenceShadow(sharedPreferences, KEY_TIMER_RUNNING, false)
-
-    private var remainingTimeMS: Long  by object : LongSharedPreferenceShadow(sharedPreferences, KEY_REMAINING_TIME_MS, SPIELZEIT_DURATION_MS) {
-        override operator fun setValue(thisRef: Any?, property: KProperty<*>, value: Long) {
-            super.setValue(thisRef, property, value)
-            fireSecondsChanged()
-        }
+    private val model = AlarmTimerModel(applicationContext) {
+        fireSecondsChanged()
     }
 
-    init {
-        if (countdownRunning) {
-            // If alarm time is in the past, the running timer already expired
-            // If alarm time is too far in the future, the saved timer is probably from before the last boot
-            if (alarmTimeMS <= SystemClock.elapsedRealtime() || alarmTimeMS > SystemClock.elapsedRealtime() + SPIELZEIT_DURATION_MS ) {
-                countdownRunning = false
-                remainingTimeMS = SPIELZEIT_DURATION_MS
-            }
-        } else {
-            if (remainingTimeMS < 0 || remainingTimeMS > SPIELZEIT_DURATION_MS) {
-                remainingTimeMS = SPIELZEIT_DURATION_MS
-            }
-        }
-    }
-
-    fun addSecondsListener(secondsChangedCallback: (Long) -> Unit) {
+     fun addSecondsListener(secondsChangedCallback: (Long) -> Unit) {
         val hadNoListeners = secondsChangedCallbacks.isEmpty()
         secondsChangedCallbacks.add(secondsChangedCallback)
-        if (hadNoListeners && countdownRunning) {
+        if (hadNoListeners && model.countdownRunning) {
             startSecondsChangeTimer()
         }
         // Give initial update to the secondsChangedCallback
-        secondsChangedCallback(computedRemainigTimeS)
+        secondsChangedCallback(model.computedRemainigTimeS)
     }
 
     fun removeSecondsListener(listener: (Long) -> Unit) {
@@ -81,33 +44,21 @@ class AlarmTimer(private val applicationContext: Context) {
     }
 
     fun startOrPauseCountdown() {
-        if (countdownRunning) {
+        if (model.countdownRunning) {
             pauseCountdown()
         } else {
-            startCountdown(computedAlarmTimeMS)
+            startCountdown(model.computedAlarmTimeMS)
         }
     }
 
     fun resetCountdown() {
         cancelSecondsChangeTimer()
         cancelAlarm()
-        remainingTimeMS = SPIELZEIT_DURATION_MS
+        model.reset()
     }
 
-    private val computedAlarmTimeMS: Long
-        get() = if (countdownRunning) alarmTimeMS else SystemClock.elapsedRealtime() + remainingTimeMS
-
-    private val computedRemainingTimeMS: Long
-        get() {
-            val computedRemainigTimeMs = if (countdownRunning) alarmTimeMS - SystemClock.elapsedRealtime() else remainingTimeMS
-            return if (computedRemainigTimeMs < 0) 0 else computedRemainigTimeMs
-        }
-
-    private val computedRemainigTimeS: Long
-        get() = (computedRemainingTimeMS + 500L).msToS()
-
     private fun pauseCountdown() {
-        remainingTimeMS = computedRemainingTimeMS
+        model.remainingTimeMS = model.computedRemainingTimeMS
         cancelAlarm()
         cancelSecondsChangeTimer()
     }
@@ -126,14 +77,14 @@ class AlarmTimer(private val applicationContext: Context) {
         } else {
             alarmManager.set(AlarmManager.ELAPSED_REALTIME_WAKEUP, newAlarmTimeMS, pendingIntent)
         }
-        alarmTimeMS = newAlarmTimeMS
-        countdownRunning = true
+        model.alarmTimeMS = newAlarmTimeMS
+        model.countdownRunning = true
     }
 
     private fun cancelAlarm() {
         val pendingIntent = getAlarmIntent(applicationContext, PendingIntent.FLAG_UPDATE_CURRENT)
         alarmManager.cancel(pendingIntent)
-        countdownRunning = false
+        model.countdownRunning = false
     }
 
     private fun cancelSecondsChangeTimer() {
@@ -144,10 +95,9 @@ class AlarmTimer(private val applicationContext: Context) {
     private fun startSecondsChangeTimer() {
         val newSecondsChangeTimer = Timer("SecondsChangeTimer", true)
         val onSecondChanged: TimerTask = object : TimerTask() {
-
             override fun run() {
                 runOnMainThread {
-                    if (computedRemainingTimeMS <= 0L) {
+                    if (model.computedRemainingTimeMS <= 0L) {
                         cancelSecondsChangeTimer()
                     }
                     fireSecondsChanged()
@@ -155,12 +105,13 @@ class AlarmTimer(private val applicationContext: Context) {
             }
 
         }
-        val timeNextSecondMS = computedRemainingTimeMS % S_IN_MS
+        val timeNextSecondMS = model.computedRemainingTimeMS % S_IN_MS
         newSecondsChangeTimer.scheduleAtFixedRate(onSecondChanged, timeNextSecondMS, S_IN_MS)
         secondsChangeTimer = newSecondsChangeTimer
     }
 
     private fun fireSecondsChanged() {
+        val computedRemainigTimeS = model.computedRemainigTimeS
         for (secondsChangedCallback in secondsChangedCallbacks) {
             secondsChangedCallback(computedRemainigTimeS)
         }
---
+++ app/src/main/java/de/musoft/elch/alarm/AlarmTimerModel.kt
@@ -0,0 +1,68 @@
+package de.musoft.elch.alarm
+
+import android.content.Context
+import android.content.SharedPreferences
+import android.os.SystemClock
+import de.musoft.elch.delegates.BooleanSharedPreferenceShadow
+import de.musoft.elch.delegates.LongSharedPreferenceShadow
+import de.musoft.elch.extensions.mToMs
+import de.musoft.elch.extensions.msToS
+import kotlin.reflect.KProperty
+
+/**
+ * Created by luckas on 21.03.17.
+ */
+
+private const val KEY_TIMER_RUNNING = "TIMER_RUNNING"
+private const val KEY_ALARM_TIME_MS = "ALARM_TIME_MS"
+private const val KEY_REMAINING_TIME_MS = "REMAINING_TIME_MS"
+
+private const val SPIELZEIT_DURATION_MIN: Long = 8L
+
+private val SPIELZEIT_DURATION_MS = SPIELZEIT_DURATION_MIN.mToMs()
+
+class AlarmTimerModel(private val applicationContext: Context, val secondsChangedCallback: () -> Unit) {
+    private val sharedPreferences: SharedPreferences = applicationContext.getSharedPreferences("TimerState", Context.MODE_PRIVATE)
+    internal var alarmTimeMS: Long by LongSharedPreferenceShadow(sharedPreferences, KEY_ALARM_TIME_MS, 0)
+    internal var countdownRunning: Boolean by BooleanSharedPreferenceShadow(sharedPreferences, KEY_TIMER_RUNNING, false)
+
+    internal var remainingTimeMS: Long  by object : LongSharedPreferenceShadow(sharedPreferences, KEY_REMAINING_TIME_MS, SPIELZEIT_DURATION_MS) {
+        override operator fun setValue(thisRef: Any?, property: KProperty<*>, value: Long) {
+            super.setValue(thisRef, property, value)
+            secondsChangedCallback()
+        }
+    }
+
+    internal val computedAlarmTimeMS: Long
+        get() = if (countdownRunning) alarmTimeMS else SystemClock.elapsedRealtime() + remainingTimeMS
+
+    internal val computedRemainingTimeMS: Long
+        get() {
+            val computedRemainigTimeMs = if (countdownRunning) alarmTimeMS - SystemClock.elapsedRealtime() else remainingTimeMS
+            return if (computedRemainigTimeMs < 0) 0 else computedRemainigTimeMs
+        }
+
+    internal val computedRemainigTimeS: Long
+        get() = (computedRemainingTimeMS + 500L).msToS()
+
+    init {
+        if (countdownRunning) {
+            // If alarm time is in the past, the running timer already expired
+            // If alarm time is too far in the future, the saved timer is probably from before the last boot
+            if (alarmTimeMS <= SystemClock.elapsedRealtime() || alarmTimeMS > SystemClock.elapsedRealtime() + SPIELZEIT_DURATION_MS ) {
+                countdownRunning = false
+                remainingTimeMS = SPIELZEIT_DURATION_MS
+            }
+        } else {
+            if (remainingTimeMS < 0 || remainingTimeMS > SPIELZEIT_DURATION_MS) {
+                remainingTimeMS = SPIELZEIT_DURATION_MS
+            }
+        }
+    }
+
+    fun reset() {
+        remainingTimeMS = SPIELZEIT_DURATION_MS
+    }
+
+}
+
