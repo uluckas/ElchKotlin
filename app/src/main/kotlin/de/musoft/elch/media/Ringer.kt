31f8abe3f6e8834e8535dbe4e873fff670d1f9d4
Uli Luckas <uli.luckas@sinnerschrader-mobile.com>
2017-04-02 02:19:22 +0200

    Convert wakelock dance to coroutine based implementation

--- app/src/main/kotlin/de/musoft/elch/media/Ringer.kt
+++ app/src/main/kotlin/de/musoft/elch/media/Ringer.kt
@@ -7,6 +7,11 @@ import android.os.PowerManager
 import de.musoft.elch.app.R
 import de.musoft.elch.extensions.powerManager
 import de.musoft.elch.extensions.uriForRessource
+import kotlinx.coroutines.experimental.android.UI
+import kotlinx.coroutines.experimental.launch
+import java.util.logging.Level
+import java.util.logging.Logger
+import kotlin.coroutines.experimental.suspendCoroutine
 
 /**
  * Created by luckas on 15.09.16.
@@ -14,45 +19,39 @@ import de.musoft.elch.extensions.uriForRessource
 
 private const val volume = 1.0f
 
-/**
- * Call prepareAsync on mediaPlayer, catch all exceptions and report success
- *
- * @param mediaPlayer The mediaplayer object
- *
- * @return true if successfull
- */
-private fun tryPrepareAsync(mediaPlayer: MediaPlayer): Boolean {
-    try {
-        mediaPlayer.prepareAsync()
-        return true
-    } catch (e: Throwable) {
-        return false
-    }
-}
-
-fun ring(context: Context) {
-    val uri = context.uriForRessource(R.raw.ring)
-    val mediaPlayer = MediaPlayer()
-    val pm = context.powerManager
-    val wakeLock = pm.newWakeLock(PowerManager.PARTIAL_WAKE_LOCK, "AlarmHandover")
-
-    mediaPlayer.setOnCompletionListener { it.release() }
-    mediaPlayer.setWakeMode(context, PowerManager.PARTIAL_WAKE_LOCK)
-    mediaPlayer.setDataSource(context, uri)
-    mediaPlayer.setAudioStreamType(AudioManager.STREAM_ALARM)
-    mediaPlayer.setVolume(volume, volume)
-    mediaPlayer.setOnPreparedListener {
-        try {
-            mediaPlayer.start()
-        } finally {
-            // No more need to hold the WakeLock
-            // While playing, the MediaPlayer holds it's own WakeLock
-            wakeLock.release()
+suspend fun MediaPlayer.prepareSuspending(): Unit =
+        suspendCoroutine<Unit> { cont ->
+            try {
+                setOnPreparedListener {
+                    cont.resume(Unit)
+                }
+                prepareAsync()
+            } catch (t: Throwable) {
+                cont.resumeWithException(t)
+            }
         }
-    }
-    if (tryPrepareAsync(mediaPlayer)) {
-        // If prepareAsync succeeded, we can be sure, the onPreparedListener will be called
-        // So proceede to acquire the WakeLock, it will be released in the onPreparedListener
-        wakeLock.acquire()
-    }
-}
+
+fun ring(context: Context) =
+        launch(UI) {
+            val pm = context.powerManager
+            val wakeLock = pm.newWakeLock(PowerManager.PARTIAL_WAKE_LOCK, "AlarmHandover")
+            wakeLock.acquire()
+            try {
+                val uri = context.uriForRessource(R.raw.ring)
+                val mediaPlayer = MediaPlayer()
+
+                mediaPlayer.setOnCompletionListener { it.release() }
+                mediaPlayer.setWakeMode(context, PowerManager.PARTIAL_WAKE_LOCK)
+                mediaPlayer.setDataSource(context, uri)
+                mediaPlayer.setAudioStreamType(AudioManager.STREAM_ALARM)
+                mediaPlayer.setVolume(volume, volume)
+                mediaPlayer.prepareSuspending()
+                mediaPlayer.start()
+            } catch (t: Throwable) {
+                val logger = Logger.getLogger(this::class.java.name)
+                logger.log(Level.SEVERE, "Could not play ring tone", t)
+            } finally {
+                wakeLock.release()
+            }
+        }
+
